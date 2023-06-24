168edf4b96e7799347ee20211c8f8109726b866e
ccalvillo22 <ccalvillo22@udavinci.edu.mx>
2023-06-24 17:50:18 -0600

    lab

--- lab-7/function.js
+++ lab-7/function.js
@@ -1,7 +1,7 @@
  
 function ingresartexto(){
   var fname = document.getElementById("nombre").value;
-  var re1 = /^[a-zA-Z\s\'\-]{2,15}$/;
+  var re1 = /^[a-zA-Z\'\-] $/;
   if(re1.test(fname)){
     document.getElementById("mensaje").style.color = "Green";
     document.getElementById("mensaje").innerHTML = "<strong>Correcto</strong>";
@@ -15,4 +15,60 @@ function ingresartexto(){
 }
 }
 
+function formulario()                                    
+{ 
+    var address = document.forms["RegForm"]["Address"]; 
+    var password = document.forms["RegForm"]["Password"];  
+    var what =  document.forms["RegForm"]["Subject"];  
+    /var email = document.forms["RegForm"]["EMail"];/    
+    var name = document.forms["RegForm"]["Name"];               
+    var what =  document.forms["RegForm"]["Subject"];  
+    var phone = document.forms["RegForm"]["Telephone"];  
+ 
+    if (name.value == "")                                  
+    { 
+        window.alert("Please enter your name."); 
+        name.focus(); 
+        return false; 
+    } 
+    if (address.value == "")                               
+    { 
+        window.alert("Please enter your address."); 
+        name.focus(); 
+        return false; 
+    } 
+    if (email.value == "") 
+
+    { 
+        window.alert("Please enter a valid e-mail address."); 
+        email.focus(); 
+        return false; 
+    } 
+    if (email.value.indexOf("@", 0) < 0)                 
+    { 
+        window.alert("Please enter a valid e-mail address."); 
+        email.focus(); 
+        return false; 
+    } 
+    if (email.value.indexOf(".", 0) < 0)                 
+    { 
+        window.alert("Please enter a valid e-mail address."); 
+        email.focus(); 
+        return false; 
+    } 
+    
+    if (phone.value == "")                           
+    { 
+        window.alert("Please enter your telephone number."); 
+        phone.focus(); 
+        return false; 
+    } 
+    if (password.value == "")                        
+    { 
+        window.alert("Please enter your password"); 
+        password.focus(); 
+        return false; 
+    } 
+    return true; 
+}
 
