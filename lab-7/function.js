9c9966a52b8956e2909f28dd13d04406868c67d4
ccalvillo22 <ccalvillo22@udavinci.edu.mx>
2023-06-24 16:08:15 -0600

    lab

--- lab-7/function.js
+++ lab-7/function.js
@@ -1,15 +1,9 @@
  
 function ingresartexto(){
   var field = document.getElementById("nombre").value;
-  var email = /^[A-Za-z]$/;
-  if(email.test(field))
-    document.getElementById("mensaje").innerHTML = "Entrada validada";
+  var input = /^[A-Za-z] $/;
+  if(input.test(field))
+    document.getElementById("mensaje").innerHTML = "Correcto";
   else
     document.getElementById("mensaje").innerHTML = "Obligatorio";
-}
-
-function formulario()                                    
-{      
-    var what =  document.forms["RegForm"]["Subject"];  
-    return true; 
 }
