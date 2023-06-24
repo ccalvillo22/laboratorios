e7713551f4a172f7b45d2d61c3204e56660b038a
ccalvillo22 <ccalvillo22@udavinci.edu.mx>
2023-06-24 15:52:37 -0600

    lab

--- lab-7/function.js
+++ lab-7/function.js
@@ -5,62 +5,11 @@ function ingresartexto(){
   if(email.test(field))
     document.getElementById("mensaje").innerHTML = "Entrada validada";
   else
-    document.getElementById("mensaje").innerHTML = "Obligatorio => Ingrese el correo electronico";
+    document.getElementById("mensaje").innerHTML = "Obligatorio";
 }
 
 function formulario()                                    
-{ 
-    var address = document.forms["RegForm"]["Address"]; 
-    var password = document.forms["RegForm"]["Password"];  
+{      
     var what =  document.forms["RegForm"]["Subject"];  
-    /var email = document.forms["RegForm"]["EMail"];/    
-    var name = document.forms["RegForm"]["Name"];               
-    var what =  document.forms["RegForm"]["Subject"];  
-    var phone = document.forms["RegForm"]["Telephone"];  
- 
-    if (name.value == "")                                  
-    { 
-        window.alert("Please enter your name."); 
-        name.focus(); 
-        return false; 
-    } 
-    if (address.value == "")                               
-    { 
-        window.alert("Please enter your address."); 
-        name.focus(); 
-        return false; 
-    } 
-    /if (email.value == "") 
-
-    { 
-        window.alert("Please enter a valid e-mail address."); 
-        email.focus(); 
-        return false; 
-    } 
-    if (email.value.indexOf("@", 0) < 0)                 
-    { 
-        window.alert("Please enter a valid e-mail address."); 
-        email.focus(); 
-        return false; 
-    } 
-    if (email.value.indexOf(".", 0) < 0)                 
-    { 
-        window.alert("Please enter a valid e-mail address."); 
-        email.focus(); 
-        return false; 
-    } 
-    /
-    if (phone.value == "")                           
-    { 
-        window.alert("Please enter your telephone number."); 
-        phone.focus(); 
-        return false; 
-    } 
-    if (password.value == "")                        
-    { 
-        window.alert("Please enter your password"); 
-        password.focus(); 
-        return false; 
-    } 
     return true; 
 }
