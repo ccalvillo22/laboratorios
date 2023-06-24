b7cc3f6cdfc689a7bdaf2c2fe712f39bf5156487
ccalvillo22 <ccalvillo22@udavinci.edu.mx>
2023-06-24 14:09:43 -0600

    lab

--- lab-7/function.js
+++ lab-7/function.js
@@ -1,43 +1,69 @@
  
- const form = document.getElementById('contact-form');
-      const nameInput = document.getElementById('name');
-      const emailInput = document.getElementById('email');
-      const messageInput = document.getElementById('message');
-      const errorMessages = document.getElementsByClassName('error');
-
-      function validateForm() {
-        // Clear any existing error messages
-        for (let i = 0; i < errorMessages.length; i++) {
-          errorMessages[i].textContent = '';
-        }
-
-        let isValid = true;
-
-        // Validate name field
-        if (nameInput.value.trim() === '') {
-          nameInput.nextElementSibling.textContent = 'Name is required';
-          isValid = false;
-        }
-
-        // Validate email field
-        const emailPattern = /^[^@]+@[^@]+\.[^@]+$/;
-        if (!emailPattern.test(emailInput.value)) {
-          emailInput.nextElementSibling.textContent = 'Please enter a valid email address';
-          isValid = false;
-        }
-
-        // Validate message field
-        if (messageInput.value.trim() === '') {
-          messageInput.nextElementSibling.textContent = 'Message is required';
-          isValid = false;
-        }
-
-        return isValid;
-      }
-
-      form.addEventListener('submit', (event) => {
-        // Prevent the form from submitting if it's invalid
-        if (!validateForm()) {
-          event.preventDefault();
-        }
-      });
+ <script> 
+function GEEKFORGEEKS()                                    
+{ 
+    var name = document.forms["RegForm"]["Name"];               
+    var email = document.forms["RegForm"]["EMail"];    
+    var phone = document.forms["RegForm"]["Telephone"];  
+    var what =  document.forms["RegForm"]["Subject"];  
+    var password = document.forms["RegForm"]["Password"];  
+    var address = document.forms["RegForm"]["Address"];  
+   
+    if (name.value == "")                                  
+    { 
+        window.alert("Please enter your name."); 
+        name.focus(); 
+        return false; 
+    } 
+   
+    if (address.value == "")                               
+    { 
+        window.alert("Please enter your address."); 
+        name.focus(); 
+        return false; 
+    } 
+       
+    if (email.value == "")                                   
+    { 
+        window.alert("Please enter a valid e-mail address."); 
+        email.focus(); 
+        return false; 
+    } 
+   
+    if (email.value.indexOf("@", 0) < 0)                 
+    { 
+        window.alert("Please enter a valid e-mail address."); 
+        email.focus(); 
+        return false; 
+    } 
+   
+    if (email.value.indexOf(".", 0) < 0)                 
+    { 
+        window.alert("Please enter a valid e-mail address."); 
+        email.focus(); 
+        return false; 
+    } 
+   
+    if (phone.value == "")                           
+    { 
+        window.alert("Please enter your telephone number."); 
+        phone.focus(); 
+        return false; 
+    } 
+   
+    if (password.value == "")                        
+    { 
+        window.alert("Please enter your password"); 
+        password.focus(); 
+        return flase; 
+    } 
+   
+    if (what.selectedIndex < 1)                  
+    { 
+        alert("Please enter your course."); 
+        what.focus(); 
+        return false; 
+    } 
+   
+    return true; 
+}</script>
