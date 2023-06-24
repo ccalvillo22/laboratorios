8757e558f361766786d6d9236e065b20deea8278
ccalvillo22 <ccalvillo22@udavinci.edu.mx>
2023-06-24 17:45:57 -0600

    lab

--- lab-7/function.js
+++ lab-7/function.js
@@ -5,13 +5,14 @@ function ingresartexto(){
   if(re1.test(fname)){
     document.getElementById("mensaje").style.color = "Green";
     document.getElementById("mensaje").innerHTML = "<strong>Correcto</strong>";
+     return true;
   }
-  return true;
-}
+ 
   else{
     document.getElementById("mensaje").style.color = "Red";
     document.getElementById("mensaje").innerHTML = "<strong>Obligatorio</strong>";
     return false;
+}
 }
 
 
