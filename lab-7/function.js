ca1a217d49c699a5a937273fea97c1a4aba620dc
ccalvillo22 <ccalvillo22@udavinci.edu.mx>
2023-06-24 18:38:42 -0600

    lab

--- lab-7/function.js
+++ lab-7/function.js
@@ -1,74 +1,84 @@
- 
-function ingresartexto(){
-  var fname = document.getElementById("nombre").value;
-  var re1 = /^[a-zA-Z\'\-] $/;
-  if(re1.test(fname)){
-    document.getElementById("mensaje").style.color = "Green";
-    document.getElementById("mensaje").innerHTML = "<strong>Correcto</strong>";
-     return true;
+/* Funcion de Nombre */
+function funcion_nombre (){
+  var name = document.getElementById("nombre").value;
+  var a = /^[a-zA-Z\s\'\-] {2,15}$/;
+
+  id (a.test(name)){
+    document.getElementById("nombrePrompt").style.color = "green";
+    document.getElementById("nombrePrompt").InnerHTML = "Ok"
+    return true;
   }
- 
-  else{
-    document.getElementById("mensaje").style.color = "Red";
-    document.getElementById("mensaje").innerHTML = "<strong>Obligatorio</strong>";
+  else {
+    document.getElementById("nombrePrompt").style.color = red
+    document.getElementById("nombrePrompt").InnerHTML = "Ingresa los caracteres correctos"
     return false;
-}
-}
-
-function formulario()                                    
-{ 
-    var address = document.forms["RegForm"]["Address"]; 
-    var password = document.forms["RegForm"]["Password"];  
-    var what =  document.forms["RegForm"]["Subject"];  
-    /var email = document.forms["RegForm"]["EMail"];/    
-    var name = document.forms["RegForm"]["Name"];               
-    var what =  document.forms["RegForm"]["Subject"];  
-    var phone = document.forms["RegForm"]["Telephone"];  
- 
-    if (name.value == "")                                  
-    { 
-        window.alert("Please enter your name."); 
-        name.focus(); 
-        return false; 
-    } 
-    if (address.value == "")                               
-    { 
-        window.alert("Please enter your address."); 
-        name.focus(); 
-        return false; 
-    } 
-    if (email.value == "") 
-
-    { 
-        window.alert("Please enter a valid e-mail address."); 
-        email.focus(); 
-        return false; 
-    } 
-    if (email.value.indexOf("@", 0) < 0)                 
-    { 
-        window.alert("Please enter a valid e-mail address."); 
-        email.focus(); 
-        return false; 
-    } 
-    if (email.value.indexOf(".", 0) < 0)                 
-    { 
-        window.alert("Please enter a valid e-mail address."); 
-        email.focus(); 
-        return false; 
-    } 
-    
-    if (phone.value == "")                           
-    { 
-        window.alert("Please enter your telephone number."); 
-        phone.focus(); 
-        return false; 
-    } 
-    if (password.value == "")                        
-    { 
-        window.alert("Please enter your password"); 
-        password.focus(); 
-        return false; 
-    } 
-    return true; 
+  }
 }
 
+/* Funcion de Direccion */
+function funcion_dir (){
+  var add = document.getElementById("direccion").value;
+  var b = /^[a-zA-Z\s\'\-] {2,15}$/;
+
+  id (b.test(add)){
+    document.getElementById("direccionPrompt").style.color = "green";
+    document.getElementById("direccionPrompt").InnerHTML = "Ok"
+    return true;
+  }
+  else {
+    document.getElementById("direccionPrompt").style.color = red
+    document.getElementById("direccionPrompt").InnerHTML = "Ingresa los caracteres correctos"
+    return false;
+  }
+}
+
+/* Funcion de E-mail */
+function funcion_dir (){
+  var eml = document.getElementById("email").value;
+  var c = /^[a-zA-Z\s\'\-] {2,15}$/;
+
+  id (c.test(eml)){
+    document.getElementById("emailPrompt").style.color = "green";
+    document.getElementById("emailPrompt").InnerHTML = "Ok"
+    return true;
+  }
+  else {
+    document.getElementById("emailPrompt").style.color = red
+    document.getElementById("emailPrompt").InnerHTML = "Ingresa los caracteres correctos"
+    return false;
+  }
+}
+
+/* Funcion de Telefono */
+function funcion_nombre (){
+  var tel = document.getElementById("telefono").value;
+  var d = /^[a-zA-Z\s\'\-] {2,15}$/;
+
+  id (d.test(tel)){
+    document.getElementById("telefonoPrompt").style.color = "green";
+    document.getElementById("telefonoPrompt").InnerHTML = "Ok"
+    return true;
+  }
+  else {
+    document.getElementById("telefonoPrompt").style.color = red
+    document.getElementById("telefonoPrompt").InnerHTML = "Ingresa los caracteres correctos"
+    return false;
+  }
+}
+
+/* Funcion de Contraseña */
+function funcion_nombre (){
+  var cont = document.getElementById("contrasena").value;
+  var e = /^[a-zA-Z\s\'\-] {2,15}$/;
+
+  id (e.test(cont)){
+    document.getElementById("contrasenaPrompt").style.color = "green";
+    document.getElementById("contrasenaPrompt").InnerHTML = "Ok"
+    return true;
+  }
+  else {
+    document.getElementById("nombrePrompt").style.color = red
+    document.getElementById("nombrePrompt").InnerHTML = "Ingresa los caracteres correctos"
+    return false;
+  }
+}
